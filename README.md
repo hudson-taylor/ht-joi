f47d3660bcaeb1a054031a4a331176fc0f6db12c
Adam Brady <adam@boxxen.org>
2015-06-15 01:59:32 +1000

    make it so

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ht-joi
-Schema wrapper for Joi
+
+This library makes Joi schemas compatible with hudson-taylor
+
+*Requires HT >6.0.0*
+
+## Example
+
+```js
+
+var joi   = require('joi');
+var htjoi = require('ht-joi');
+
+// setup your ht service and stuff here
+
+var schema = htjoi(joi.string().required());
+
+service.on("echo", schema, function(data, callback) {
+  return callback(null, data);
+});
+
+```
+
+## License
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,59 @@
+
+var assert = require('assert');
+var ht     = require('hudson-taylor');
+var joi    = require('joi');
+
+var htJoi = require('../lib');
+
+describe("HT Joi", function() {
+
+  var service, client;
+
+  before(function() {
+
+    var transport = new ht.Transports.Local();
+
+    service = new ht.Service(transport);
+
+    var schema = htJoi(joi.object().keys({
+      username: joi.string().min(2).max(5).required(),
+      password: joi.string().min(8).required()
+    }));
+
+    service.on("login", schema, function(data, callback) {
+      return callback(null, data);
+    });
+
+    client = new ht.Client({
+      s1: transport
+    });
+
+  });
+
+  it("should wrap joi schemas", function(done) {
+
+    client.call("s1", "login", {
+      username: "hello",
+      password: "world1234"
+    }, function(err, data) {
+      assert.ifError(err);
+      assert.equal(data.username, "hello");
+      assert.equal(data.password, "world1234");
+      done();
+    });
+
+  });
+
+  it("should properly return errors", function(done) {
+
+    client.call("s1", "login", {
+      hello: "world"
+    }, function(err) {
+      assert.equal(err.$htValidationError, true);
+      assert.equal(err.error, 'child "username" fails because ["username" is required]');
+      done();
+    });
+
+  });
+
+});
